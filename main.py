d01e04c5fd0178d3c1331a656ae0cbb16c4f5adf
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-28 21:07:08 +0500

    To Prettify the Content Html

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup)
