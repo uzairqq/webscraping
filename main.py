67fe2762e6b03d3fd9195a8eae0603e2d29adf3b
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-28 23:42:30 +0500

    Filter With Publish Date

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ soup = BeautifulSoup(html_content, 'lxml')
 jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
 
 for job in jobs:
-    company_name = job.find(
-        'h3', class_="joblist-comp-name").text.strip()
-    job_skills = job.find(
-        'span', class_="srp-skills").text.strip().replace(' ', '')
     job_posted = job.find(
         'span', class_="sim-posted").text.strip()
+    if 'few' not in job_posted:
+        company_name = job.find(
+            'h3', class_="joblist-comp-name").text.strip()
+        job_skills = job.find(
+            'span', class_="srp-skills").text.strip().replace(' ', '')
 
-    print(f'''
-    Company Name: {company_name}
-    Skills Needed: {job_skills}
-    Job Posted: {job_posted}
-    ''')
+        print(f'''
+        Company Name: {company_name}
+        Skills Needed: {job_skills}
+        Job Posted: {job_posted}
+        ''')
 
-    print('')
+        print('')
