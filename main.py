f70c9af0ae7cf6c686fab91b1fb5690f5b1a86d4
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-28 23:35:26 +0500

    Looping Through Different Jobs

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import requests
 html_content = requests.get(
     'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_content, 'lxml')
-job = soup.find('li', class_="clearfix job-bx wht-shd-bx")
-company_name = job.find(
-    'h3', class_="joblist-comp-name").text.strip()
-job_skills = job.find(
-    'span', class_="srp-skills").text.strip().replace(' ', '')
-job_posted = job.find(
-    'span', class_="sim-posted").text.strip()
+jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
 
+for job in jobs:
+    company_name = job.find(
+        'h3', class_="joblist-comp-name").text.strip()
+    job_skills = job.find(
+        'span', class_="srp-skills").text.strip().replace(' ', '')
+    job_posted = job.find(
+        'span', class_="sim-posted").text.strip()
 
-print(f'''
-Company Name: {company_name}
-Skills Needed: {job_skills}
-Job Posted: {job_posted}
-''')
+    print(f'''
+    Company Name: {company_name}
+    Skills Needed: {job_skills}
+    Job Posted: {job_posted}
+    ''')
+
+    print('')
