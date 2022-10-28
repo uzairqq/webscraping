cc7d2adaf64ca83f1f84beac449ddd494e2ba4aa
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-28 22:06:55 +0500

    Using the Requests Library To See a Websites Html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_content = requests.get('')
-
+html_content = requests.get(
+    'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
+print(html_content)
