444556bdea09d3bf1e45861113cea75c69892b62
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-28 21:37:10 +0500

    Grabing All the Cards OF Html

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from asyncore import read
 from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find('h5')
-    tagsWithText = soup.find('h5').text
-    print(tags)
-    print(tagsWithText)
 
-    findsAllTags = soup.find_all('h5')
-    for course in findsAllTags:
-        print(course.text)
+    all_course_cards = soup.find_all('div', class_='card')
+    for course_card in all_course_cards:
+        print(course_card)
